b4f1e26069b18108bde654ec301f327f183881b0
XiaoYuZe <zamorlq@163.com>
2016-04-26 11:49:09 +0800

    201604261140

--- gsme/DBClient.py
+++ gsme/DBClient.py
@@ -2,6 +2,6 @@
 
 import pyodbc
 
-database_client_connection = pyodbc.connect('DRIVER={SQL SERVER};SERVER=114.215.140.37;DATABASE=pachong;UID=likai;PWD=$qhz,gS*Z6S#3N"*')
+database_client_connection = pyodbc.connect('DRIVER={SQL SERVER};SERVER=172.16.0.26;DATABASE=pachong;UID=zhangxiaogang;PWD=29*YIH&Osajf>')
 # database_client_connection = pyodbc.connect('DRIVER={SQL SERVER};SERVER=LENOVO-LK\MSSQL;DATABASE=pachong;UID=sa;PWD=LiKai0129')
 database_client_cursor = database_client_connection.cursor()
